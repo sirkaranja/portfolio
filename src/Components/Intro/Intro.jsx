3b1b1219f278fdf618564e2164cf39cb1dcbe05a
Daniel Karanja <sirkaranja12@gmail.com>
2023-12-03 17:14:33 +0300

    intro section

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div className="intro-text ">
+    <h1>Hi, I'm Dan</h1>
+    <p>A full-stack developer, web developer, API expert</p>
+    <p>I develop beautiful and responsive SEO optimized websites.</p>
+  </div>
+  )
+}
+
+export default Intro
