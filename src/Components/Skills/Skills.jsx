1404b57241470a7688eb828f6b06581280d909e0
Daniel Karanja <sirkaranja12@gmail.com>
2023-12-03 19:02:39 +0300

    skills component

---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Skills() {
+  return (
+    <div className="skills-main">
+        <div className="front-dev">
+            <h3>Frontend Developemnt</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Skills
