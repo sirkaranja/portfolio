2b366b348593b116502c1026863092fc5e6892a0
Daniel Karanja <sirkaranja12@gmail.com>
2023-12-03 22:03:36 +0300

    wrapped card with new div and styling it

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import './Services.css';
 
 function Services() {
   return (
-    <div>Services</div>
-  )
+    <div className="main-serv">
+      <h3>Services</h3>
+      <div className="services-container">
+        <div className="info">
+          <h3>Web Development</h3>
+          <p>Creating responsive and user-friendly websites using modern technologies.</p>
+        </div>
+        <div className="info">
+          <h3>Backend Development</h3>
+          <p>Building server-side applications and APIs using various technologies.</p>
+        </div>
+        <div className="info">
+          <h3>Frontend Development</h3>
+          <p>Creating interactive and responsive user interfaces for the web.</p>
+        </div>
+        <div className="info">
+          <h3>UI/UX Design</h3>
+          <p>Designing intuitive and user-friendly interfaces for digital products.</p>
+        </div>
+        <div className="info">
+          <h3>Graphics Design</h3>
+          <p>Creating visually appealing graphics and artwork for various purposes.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Services
+export default Services;
