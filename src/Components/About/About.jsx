638942709b4be4466abdb0dd6ca09dc6d863eecb
Daniel Karanja <sirkaranja12@gmail.com>
2023-12-03 18:18:49 +0300

    about details

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function About() {
+  return (
+   <div className="main-about">
+    <div className="img">
+        <img src="" alt="" srcset="" />
+    </div>
+    <div className="details">
+        <h1>About Me</h1>
+        <p>With over 4+ years experience in building professional websites for businesses, brands and individuals, <br/>I bring you closer to your customers with my developer + SEO skills.
+
+Got a project? Hire me and enjoy quality & excellence in delivery.</p>
+    </div>
+   </div>
+  )
+}
+
+export default About
