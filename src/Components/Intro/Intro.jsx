168dce3c3a0bce6faf76a00f0ce863e4b03ccb90
Daniel Karanja <sirkaranja12@gmail.com>
2023-12-03 18:12:53 +0300

    button styling and scrolling icon

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
+import './Intro.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import {
+    faGithub,
+    faMedium,
+    faInstagram,
+    faFacebook,
+    faTwitter,
+  } from '@fortawesome/free-brands-svg-icons';
+  import { faDesktop  } from '@fortawesome/free-solid-svg-icons';
+  import { Link } from 'react-scroll';
 function Intro() {
   return (
-    <div className="intro-text ">
-    <h1>Hi, I'm Dan</h1>
-    <p>A full-stack developer, web developer, API expert</p>
-    <p>I develop beautiful and responsive SEO optimized websites.</p>
+    <div className="intro-text">
+    
+    <div className="social-icons">
+    <a href="https://github.com/yourusername">
+          <FontAwesomeIcon icon={faGithub} className="icon" />
+        </a>
+        <a href="https://medium.com/yourusername">
+          <FontAwesomeIcon icon={faMedium} className="icon" />
+        </a>
+        <a href="https://instagram.com/yourusername">
+          <FontAwesomeIcon icon={faInstagram} className="icon" />
+        </a>
+        <a href="https://facebook.com/yourusername">
+          <FontAwesomeIcon icon={faFacebook} className="icon" />
+        </a>
+        <a href="https://twitter.com/yourusername">
+          <FontAwesomeIcon icon={faTwitter} className="icon" />
+        </a>
+        
+    </div>
+    <div className="text-content">
+      <h1>Hi, I'm Dan</h1>
+      <div className="intro-paragraphs">
+        <p>A full-stack developer, web developer, API expert</p>
+        <p>I develop beautiful and responsive SEO optimized websites.</p>
+        <button className='btn'>Hire Me</button>
+      </div>
+      <div className="scroll-down">
+        <Link to="sectionToScrollTo" smooth={true} duration={500}>
+          <FontAwesomeIcon icon={faDesktop } className="icon" />
+        </Link>
+      </div>
+    </div>
   </div>
   )
 }
