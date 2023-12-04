054c10d8ff8be685204b201dd7fbce2c1eb524d5
Daniel Karanja <sirkaranja12@gmail.com>
2023-12-04 20:59:18 +0300

    portfolio links and descriptions

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Portfolio.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faGithub} from '@fortawesome/free-brands-svg-icons';
 function Portfolio() {
   return (
    <div className="main-port">
@@ -21,6 +21,39 @@ function Portfolio() {
         <div className="project-info">
         <img src="https://shorturl.at/bzFGT" alt="" />
           <h3>Events Management System</h3>
+          <p>Creating responsive and user-friendly websites using modern technologies.</p>
+          <a href="https://github.com/yourusername">Github
+          <FontAwesomeIcon icon={faGithub} className="icon" />
+        </a>
+        <a href="https://github.com/yourusername">Live Demo
+          <FontAwesomeIcon icon={faGithub} className="icon" />
+        </a>
+        </div>
+        <div className="project-info">
+        <img src="https://shorturl.at/bzFGT" alt="" />
+          <h3>Uber clone</h3>
+          <p>Creating responsive and user-friendly websites using modern technologies.</p>
+          <a href="https://github.com/yourusername">Github
+          <FontAwesomeIcon icon={faGithub} className="icon" />
+        </a>
+        <a href="https://github.com/yourusername">Live Demo
+          <FontAwesomeIcon icon={faGithub} className="icon" />
+        </a>
+        </div>
+        <div className="project-info">
+        <img src="https://shorturl.at/bzFGT" alt="" />
+          <h3>Uber clone</h3>
+          <p>Creating responsive and user-friendly websites using modern technologies.</p>
+          <a href="https://github.com/yourusername">Github
+          <FontAwesomeIcon icon={faGithub} className="icon" />
+        </a>
+        <a href="https://github.com/yourusername">Live Demo
+          <FontAwesomeIcon icon={faGithub} className="icon" />
+        </a>
+        </div>
+        <div className="project-info">
+        <img src="https://shorturl.at/bzFGT" alt="" />
+          <h3>Uber clone</h3>
           <p>Creating responsive and user-friendly websites using modern technologies.</p>
           <a href="https://github.com/yourusername">Github
           <FontAwesomeIcon icon={faGithub} className="icon" />
