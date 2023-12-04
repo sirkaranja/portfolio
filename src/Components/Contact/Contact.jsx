1cfba94da5ebc4797adcd97235ec163263966d23
Daniel Karanja <sirkaranja12@gmail.com>
2023-12-04 21:01:45 +0300

    contact form

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Contact() {
+  return (
+   <div className="main-contact">
+    <h3>Contact Us</h3>
+    <div className="title">
+        
+    </div>
+   </div>
+  )
+}
+
+export default Contact
