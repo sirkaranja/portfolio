a78d1ee3184f21724cc10a6d02a212a212508ab4
Daniel Karanja <sirkaranja12@gmail.com>
2023-12-10 18:29:38 +0300

    aligned content to flex

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
-import './About.css'
+import React from 'react';
+import './About.css';
+
 function About() {
   return (
-   <><div className="main-about">
-          <div className="img-d">
-              <img src=" https://shorturl.at/jlxzC " alt="img" srcset="" />
-              
+    <div className="main-about">
+      <div className="img-container">
+        <img src="https://wiki.tripwireinteractive.com/TWIimages/4/47/Placeholder.png" alt="Your descriptive alt text" />
+      </div>
+      <div className="details-container">
+        <div className="details">
+          <h1>About Me</h1>
+          <p>
+            With over 4+ years of experience in building <br/>professional websites for businesses, <br/>
+            brands, and individuals, I bring you closer <br/> to your customers with my developer <br/> + SEO skills.
             
+          </p>
+          <p>Got a project? Hire me and enjoy quality <br/> & excellence in delivery.</p>
+        </div>
+        <div className="details-counter">
+        <div className="counter-item">
+            <h4 className="big-number">02+</h4>
+            <span>Years of Experience</span>
           </div>
-          <div className="details">
-              <h1>About Me</h1>
-              <p>With over 4+ years experience in building professional websites for businesses, <br />brands and individuals,I bring you closer to your customers with my developer  <br />+ SEO skills.
-
-                  Got a project? Hire me and enjoy quality & excellence in delivery.</p>
+          <div className="counter-item">
+            <h4 className="big-number">12</h4>
+            <span>Completed Projects</span>
           </div>
-          <div className="details-counter">
-
-              <h4>02+ <span>Years of Experience</span></h4>
-              <h4>12 <span>Completed Projects</span></h4>
-          </div>
-      </div><button className='btn-cv'>Downlaod CV</button></>
-  )
+        </div>
+        <button className="btn-cv">Download CV</button>
+      </div>
+      
+    </div>
+  );
 }
 
-export default About
+export default About;
