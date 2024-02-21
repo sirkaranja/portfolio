192c967009c6692e0f54148de6f6284076a12b65
Daniel Karanja <sirkaranja12@gmail.com>
2024-02-21 22:31:32 +0300

    navbar linking

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './Navbar.css'; 
+import './Navbar.css';
+
+import About from '../About/About'
 
 const Navbar = () => {
   return (
@@ -10,7 +12,7 @@ const Navbar = () => {
       <div className="nav-links">
         <ul>
           <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
+          <li><a href={{About}}>About</a></li>
           <li><a href="#skills">Skills</a></li>
           <li><a href="#services">Services</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
