b263c57333a7e1c8c16b2f4aeaeec3be5ee2d394
Daniel Karanja <sirkaranja12@gmail.com>
2024-02-26 21:29:04 +0300

    footer css

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import './Footer.css'
+// Footer.js
+import React from 'react';
+import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
-  return (
-   <div className="footer">
-    <h4>Daniel Karanja</h4>
-       <div className="details">
-        <h4>Links</h4>
-        <p>Home</p>
-        <p>About</p>
-        <p>Portfolio</p>
-        <p>Contact</p>
-       </div>
-        <div className="social-icon">
-        <a href="https://www.facebook.com"><FontAwesomeIcon icon={faFacebook} /></a>
-        <a href="https://www.twitter.com"><FontAwesomeIcon icon={faTwitter} /></a>
-        <a href="https://www.instagram.com"><FontAwesomeIcon icon={faInstagram} /></a>
+    return (
+        <div className="footer">
+            <h4>Daniel Karanja</h4>
+            <div className="details">
+                <h4>Links</h4>
+                <p>Home</p>
+                <p>About</p>
+                <p>Portfolio</p>
+                <p>Contact</p>
+            </div>
+            <div className="social-icon">
+                <a href="https://www.facebook.com"><FontAwesomeIcon icon={faFacebook} /></a>
+                <a href="https://www.twitter.com"><FontAwesomeIcon icon={faTwitter} /></a>
+                <a href="https://www.instagram.com"><FontAwesomeIcon icon={faInstagram} /></a>
+            </div>
+            <div className="cpy">
+                <p>&copy; 2023 Your Website. All rights reserved.</p>
+            </div>
         </div>
-      <div className="cpy">
-      <p>&copy; 2023 Your Website. All rights reserved.</p>
-      </div>
-     
-   </div>
-  )
+    );
 }
 
-export default Footer
+export default Footer;
